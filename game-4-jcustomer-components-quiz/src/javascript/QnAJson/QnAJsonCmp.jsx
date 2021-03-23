4b91a4e0ce3ac564c609077de0d23f1413751f1d
Hervé Duchesne <hduchesne@jahia.com>
2021-03-23 12:32:16 +0100

    Start to include a custom Editor for answer. -> write editor properly IN PROGRESS
    Built in progress need to deploy the code in REACTBuild in the javascript module
    
    Note: no way to rewrite field label of tiers mixin like jcr_title or others
    
    Ask4Persona add call event to sync the email, now need to get the email for the cxs -> To be continued
    
    CSS issue, compile without BS4 import. This suppose to have BS4 in website !
    
    TODO : Voir la mise en place d'un composant heading qui permet de régler la navigation dans content template
    TODO : possiblement mettre des thèmes

--- game-4-jcustomer-components-quiz/src/javascript/QnAJson/QnAJsonCmp.jsx
+++ game-4-jcustomer-components-quiz/src/javascript/QnAJson/QnAJsonCmp.jsx
@@ -2,13 +2,36 @@ import React from 'react';
 import * as PropTypes from 'prop-types';
 import './QnAJsonCmp.css';
 
-const QnAJsonCmp = ({id, value, onChange}) => {
+import {Input} from '@jahia/design-system-kit';
+
+const QnAJsonCmp = ({field, id, value, onChange}) => {
+    const maxLength = field.selectorOptions.find(option => option.name === 'maxLength');
+    //TODO create JSON and migration of existing '[*] fffff'
+    const controlledValue = value === undefined ? {} : JSON.parse(value)
+    const handleChangeLabel = (e) => {
+        controlledValue.label = e?.target?.value;
+        onChange(controlledValue);
+    }
     return (
-        <h1 id={id} onClick={onChange}>le code : {value}</h1>
+        <Input
+            fullWidth
+            id={id}
+            name={id}
+            inputProps={{
+                'aria-labelledby': `${field.name}-label`,
+                'aria-required': field.mandatory,
+                maxlength: maxLength && maxLength.value
+            }}
+            value={controlledValue.label}
+            readOnly={field.readOnly}
+            type="text"
+            onChange={handleChangeLabel}
+        />
     );
 };
 
 QnAJsonCmp.propTypes = {
+    field: PropTypes.object,
     id: PropTypes.string.isRequired,
     value: PropTypes.string,
     onChange: PropTypes.func.isRequired
--- game-4-jcustomer-components-quiz/src/javascript/init.js
+++ game-4-jcustomer-components-quiz/src/javascript/init.js
@@ -5,7 +5,7 @@ export default function () {
     registry.add('callback', 'QnAJsonEditor', {
         targets: ['jahiaApp-init:20'],
         callback: () => {
-            registry.add('selectorType', 'QnAJson', {cmp: QnAJsonCmp, supportMultiple: true});
+            registry.add('selectorType', 'QnAJson', {cmp: QnAJsonCmp, supportMultiple: false});
             console.debug('%c QnAJson Editor Extensions  is activated', 'color: #3c8cba');
         }
     });
